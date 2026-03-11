578f2c374875446ad6a232ac86879aef7074adee
damir-ilyasov <damir2006ilyasov@gmail.com>
2026-03-11 16:32:15 +0300

    Register and login completely

--- Logistics-Transportation/Controllers/AuthController.cs
+++ Logistics-Transportation/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
 ﻿using Logistics_Transportation.Security;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Logistics_Transportation.Models;
 
 namespace Logistics_Transportation.Controllers
 {
@@ -10,18 +12,54 @@ namespace Logistics_Transportation.Controllers
     [Route("api/auth")]
     public class AuthController : ControllerBase
     {
-        [HttpPost("{login}")]
-        public IActionResult AuthUserByLogin(string login)
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(UserManager<User> userManager)
         {
-            var claims = new List<Claim> { new Claim(ClaimTypes.Name, login) };
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromQuery] string email, [FromQuery] string password, [FromQuery] string phone)
+        {
+            var user = new User { UserName = email, Email = email, PhoneNumber = phone};
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("Пользователь успешно зарегистрирован");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromQuery] string email, [FromQuery] string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return Unauthorized("Пользователь не найден");
+            }
+            var passwordCorrect = await _userManager.CheckPasswordAsync(user, password);
+            if (!passwordCorrect)
+            {
+                return Unauthorized("Неверный пароль");
+            }
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
             var jwt = new JwtSecurityToken(
                 issuer: AuthOptions.ISSUER,
                 audience: AuthOptions.AUDIENCE,
                 claims: claims,
-                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
-                signingCredentials: new
-                    SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+                expires: DateTime.UtcNow.Add(TimeSpan.FromHours(2)),
+                signingCredentials: new SigningCredentials(
+                    AuthOptions.GetSymmetricSecurityKey(),
                     SecurityAlgorithms.HmacSha256));
+
             return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
         }
     }
---
+++ Logistics-Transportation/Migrations/20260311131314_RemovePhone.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logistics_Transportation.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Phone",
+                table: "AspNetUsers",
+                column: "Phone",
+                unique: true);
+        }
+    }
+}
